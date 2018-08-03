f5ed101d94b125a9167bee5cfbae3ecc15e98e92
gundam\Boris <alocate_s@hotmail.com>
2018-08-03 17:25:25 -0500

    Prueba Drag
    Prueba para controlar los toques moviendo la esfera

---
+++ PolloDemoAndroid/Assets/PolloProyect/Scripts/pruCameraFollow.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pruCameraFollow : MonoBehaviour {
+
+    public Transform Target;
+    public float SmoothSpeed;
+    public Vector3 Offset;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    //private void LateUpdate()
+    //{
+    //    Vector3 desiredPosition = Target.position + Offset;
+    //    Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, SmoothSpeed * Time.deltaTime);
+    //    transform.position = smoothedPosition;
+    //    transform.LookAt(Target);
+
+    //}
+
+    private void FixedUpdate()
+    {
+        Vector3 desiredPosition = Target.position + Offset;
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, SmoothSpeed * Time.deltaTime);
+        transform.position = smoothedPosition;
+        transform.LookAt(Target);
+    }
+}
---
+++ PolloDemoAndroid/Assets/PolloProyect/Scripts/pruDrag.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pruDrag : MonoBehaviour {
+    Vector3 touchPosition;
+    Touch _touch;
+     Rigidbody rb;
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.touchCount > 0) {
+            _touch = Input.GetTouch(0);
+
+            if (_touch.phase == TouchPhase.Began) {
+                print(_touch.position);
+                touchPosition = Camera.main.ScreenToWorldPoint(_touch.position);
+                rb.position = new Vector3(touchPosition.x, 1f, rb.position.z);
+            }
+        }
+
+	}
+}
